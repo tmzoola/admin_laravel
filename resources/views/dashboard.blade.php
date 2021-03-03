d8ce36877d50efba849fc5a3e3e0c5fa4c378bf5
Murodjon Toxirov <Murodjon.Toxirov@corp.universalbank.uz>
2021-03-03 16:03:21 +0500

    Free Admin Panel

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -51,7 +51,7 @@
             <div class="card card-chart">
                 <div class="card-header">
                     <h5 class="card-category">Last Month Income</h5>
-                    <h3 class="card-title"><i class="tim-icons icon-money-coins text-primary"></i>{{ format_money($semesterincomes) }}</h3>
+                    <h3 class="card-title"><i class="tim-icons icon-money-coins text-primary"></i></h3>
                 </div>
                 <div class="card-body">
                     <div class="chart-area">
@@ -64,7 +64,7 @@
             <div class="card card-chart">
                 <div class="card-header">
                     <h5 class="card-category">Monthly Balance</h5>
-                    <h3 class="card-title"><i class="tim-icons icon-bank text-info"></i> {{ format_money($monthlybalance) }}</h3>
+                    <h3 class="card-title"><i class="tim-icons icon-bank text-info"></i> </h3>
                 </div>
                 <div class="card-body">
                     <div class="chart-area">
@@ -77,7 +77,7 @@
             <div class="card card-chart">
                 <div class="card-header">
                     <h5 class="card-category">Expenditures Last Month</h5>
-                    <h3 class="card-title"><i class="tim-icons icon-paper text-success"></i> {{ format_money($semesterexpenses) }}</h3>
+                    <h3 class="card-title"><i class="tim-icons icon-paper text-success"></i> </h3>
                 </div>
                 <div class="card-body">
                     <div class="chart-area">
@@ -88,187 +88,7 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-lg-6 col-md-12">
-            <div class="card card-tasks">
-                <div class="card-header">
-                    <div class="row">
-                        <div class="col-8">
-                            <h4 class="card-title">Pending Sales</h4>
-                        </div>
-                        <div class="col-4 text-right">
-                            <a href="{{ route('sales.create') }}" class="btn btn-sm btn-primary">New Sale</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <div class="table-full-width table-responsive">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>
-                                        Date
-                                    </th>
-                                    <th>
-                                        Client
-                                    </th>
-                                    <th>
-                                        Products
-                                    </th>
-                                    <th>
-                                        Paid out
-                                    </th>
-                                    <th>
-                                        Total
-                                    </th>
-                                    <th>
 
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($unfinishedsales as $sale)
-                                    <tr>
-                                        <td>{{ date('d-m-y', strtotime($sale->created_at)) }}</td>
-                                        <td><a href="">{{ $sale->client->name }}<br>{{ $sale->client->document_type }}-{{ $sale->client->document_id }}</a></td>
-                                        <td>{{ $sale->products->count() }}</td>
-                                        <td>{{ format_money($sale->transactions->sum('amount')) }}</td>
-                                        <td>{{ format_money($sale->products->sum('total_amount')) }}</td>
-                                        <td class="td-actions text-right">
-                                            <a href="{{ route('sales.show', ['sale' => $sale]) }}" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="View Sale">
-                                                <i class="tim-icons icon-zoom-split"></i>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-6 col-md-12">
-            <div class="card card-tasks">
-                <div class="card-header">
-                <div class="row">
-                        <div class="col-8">
-                            <h4 class="card-title">Latest Transactions</h4>
-                        </div>
-                        <div class="col-4 text-right">
-                            <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#transactionModal">
-                                New Transaction
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <div class="table-full-width table-responsive">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>
-                                        Category
-                                    </th>
-                                    <th>
-                                        Title
-                                    </th>
-                                    <th>
-                                        Medium
-                                    </th>
-                                    <th>
-                                        Total
-                                    </th>
-                                    <th>
-
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-
-                                @foreach($lasttransactions as $transaction)
-                                    <tr> 
-                                        <td>
-                                            @if($transaction->type == 'expense')
-                                                Expense
-                                            @elseif($transaction->type == 'sale')
-                                                Sale
-                                            @elseif($transaction->type == 'payment')
-                                                Payment
-                                            @elseif($transaction->type == 'income')
-                                                Income
-                                            @else
-                                                {{ $transaction->type }}
-                                            @endif
-                                            
-                                        </td>
-                                        <td>{{ $transaction->title }}</td>
-                                        <td>{{ $transaction->method->name }}</td>
-                                        <td>{{ format_money($transaction->amount) }}</td>
-                                        <td class="td-actions text-right">
-                                            @if ($transaction->sale_id)
-                                                <a href="{{ route('sales.show', $transaction->sale_id) }}" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="More details">
-                                                    <i class="tim-icons icon-zoom-split"></i>
-                                                </a>
-                                            @endif
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="modal fade" id="transactionModal" tabindex="-1" role="dialog" aria-labelledby="transactionModal" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">New Transaction</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="d-flex justify-content-between">
-                        <a href="{{ route('transactions.create', ['type' => 'payment']) }}" class="btn btn-sm btn-primary">Payment</a>
-                        <a href="{{ route('transactions.create', ['type' => 'income']) }}" class="btn btn-sm btn-primary">Income</a>
-                        <a href="{{ route('transactions.create', ['type' => 'expense']) }}" class="btn btn-sm btn-primary">Expense</a>
-                        <a href="{{ route('sales.create') }}" class="btn btn-sm btn-primary">Sale</a>
-                        <a href="{{ route('transfer.create') }}" class="btn btn-sm btn-primary">Transfer</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 @endsection
 
-@push('js')
-    <script src="{{ asset('assets/js/plugins/chartjs.min.js') }}"></script>
-    
-    <script>
-        var lastmonths = [];
 
-        @foreach ($lastmonths as $id => $month)
-            lastmonths.push('{{ strtoupper($month) }}')
-        @endforeach
-
-        var lastincomes = {{ $lastincomes }};
-        var lastexpenses = {{ $lastexpenses }};
-        var anualsales = {{ $anualsales }};
-        var anualclients = {{ $anualclients }};
-        var anualproducts = {{ $anualproducts }};
-        var methods = [];
-        var methods_stats = [];
-
-        @foreach($monthlybalancebymethod as $method => $balance)
-            methods.push('{{ $method }}');
-            methods_stats.push('{{ $balance }}');
-        @endforeach
-
-        $(document).ready(function() {
-            demo.initDashboardPageCharts();
-        });
-    </script>
-@endpush
